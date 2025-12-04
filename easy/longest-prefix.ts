b3b643b4b8a24bbfb1ea1e4876ffc366f6f45c51
SamuelJacobsenB <samueljb.dev@gmail.com>
2025-12-04 11:32:18 -0300

    Longest Commun Prefix

--- easy/longest-prefix.ts
+++ easy/longest-prefix.ts
@@ -1,1 +1,15 @@
-//todo
+function longestCommonPrefix(strs: string[]): string {
+  let prefix: string[] = strs[0].split("");
+
+  for (let idx = 1; idx < strs.length; idx++) {
+    const strArr = strs[idx].split("");
+    for (let i = 0; i < prefix.length; i++) {
+      if (strArr[i] != prefix[i]) {
+        prefix = prefix.slice(0, i);
+        break;
+      }
+    }
+  }
+
+  return prefix.join("");
+}
