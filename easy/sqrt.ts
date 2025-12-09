6d46daf31a5b4ddad9bb31801ca0b1a474090b6d
SamuelJacobsenB <samueljb.dev@gmail.com>
2025-12-09 18:57:26 -0300

    Climb Stairs

--- easy/sqrt.ts
+++ easy/sqrt.ts
@@ -1,10 +1,3 @@
 function mySqrt(x: number): number {
-  let num = 0;
-
-  while (true) {
-    if (num * num > x) break;
-    num += 0.1;
-  }
-
-  return Math.floor(num);
+  return Math.floor(Math.sqrt(x));
 }
