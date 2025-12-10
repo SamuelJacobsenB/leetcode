ed5007816ce84ecfb019639fd1b5db8bbcc4edbd
SamuelJacobsenB <samueljb.dev@gmail.com>
2025-12-10 18:23:28 -0300

    Max Depth

---
+++ easy/max-depth.ts
@@ -0,0 +1,19 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  const left = maxDepth(root.left);
+  const right = maxDepth(root.right);
+
+  return left > right ? left + 1 : right + 1;
+}
